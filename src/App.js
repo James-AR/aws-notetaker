d71eef1810e9affccb13e9c51bd9d3d2426cdb35
James <ruiz.james12@yahoo.com>
2019-05-06 09:55:50 -0700

    Basic styling + setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { withAuthenticator } from 'aws-amplify-react'
 
-function App() {
-  return (
-    <div className="App">
-      App
-    </div>
-  );
+class App extends Component {
+  state = {
+    notes: [
+      {
+      id: 1,
+      note: "Hiya"
+      }
+    ],
+  }
+
+  render() {
+    const {notes} = this.state
+
+    return (
+      <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
+        <h1 className="code f2-1">Amplify Notetaker</h1>
+        <form className="mb3">
+          <input type="text" className="pa2 f4" placeholder="Write your note" />
+          <button className="pa 2 f4" type="submit" >Add Note</button>
+        </form>
+
+        <div>
+          {notes.map(note => (
+            <div key={note.id} className="flex items-center" >
+              <li className="list pa1 f3" >
+                {note.note}
+              </li>
+              <button className="bg-transparent bn f4" >
+                <span>&times;</span>
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default withAuthenticator(App, {
